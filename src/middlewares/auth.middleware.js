155c4bc94f2999aebc46fec2235856d25fb8da18
Cesar Ortiz Mena <mena@iMac.attlocal.net>
2025-05-12 16:14:44 -0500

    setup: clean tsconfig, output to dist/, ready for Railway

--- src/middlewares/auth.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authenticate = void 0;
-const authenticate = (req, res, next) => {
-    // Aquí podrías validar token o sesión más adelante
-    console.log('Authentication middleware passed');
-    next();
-};
-exports.authenticate = authenticate;
